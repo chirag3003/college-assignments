20dd7c5bcc43f1254f95fa0543c401ad99d3a35d
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-10-23 06:10:26 +0530

    test

--- ass/miniproject/rmdir.asm
+++ ass/miniproject/rmdir.asm
@@ -20,6 +20,7 @@ _start:
     ; Get directory name from command line argument
     pop r8
     pop r8
+    pop r8
     mov esi, dir_name
     up_dname: 
         mov al, [r8]
@@ -33,7 +34,7 @@ _start:
         mov byte [esi], 0
 
     ; Rmdir (delete) the directory
-    opr 84, dir_name, 0, 0, 0  ; 84 is the syscall number for rmdir
+    opr 84, dir_name, 0, 0  ; 84 is the syscall number for rmdir
 
     ; Exit the program
-    opr 60, 0, 0, 0, 0  ; 60 is the syscall number for exit
+    opr 60, 0, 0,  0  ; 60 is the syscall number for exit
